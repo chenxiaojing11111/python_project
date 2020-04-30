7b5625d47aedfc71b61656f1291bf43e549fc557
unknown <839558173@qq.com>
2020-04-30 15:41:04 +0800

    初始化代码仓库

---
+++ day01/test.py
@@ -0,0 +1,15 @@
+
+n = int(input("Enter the number of rows: "))
+i = 1
+while i <= n:
+    print("*" * i)
+    i += 1
+
+
+i = 0
+while i < 5:
+    j = 0
+    while j < 3:
+        print(j)
+        j += 1
+    i += 1
