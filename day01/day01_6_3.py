2898bf9225de8c91ce6e0222419d688a0c5308b8
cxj <839558173@qq.com>
2020-05-02 11:30:50 +0800

    add file1

---
+++ day01/day01_6_3.py
@@ -0,0 +1,24 @@
+'''
+********************
+Author: cxj
+Date: 2020-04-30
+********************
+'''
+
+# and逻辑与：两个数据必须都为True，才返回True
+r = True and True
+print(r)
+r = True and False
+print(r)
+
+# 逻辑或：只要两个有一个为True，结果就为True
+r = True or False
+print(r)
+r = False or False
+print(r)
+
+# not逻辑非：取反操作
+r = not True
+print(r)
+r = not False
+print(r)
